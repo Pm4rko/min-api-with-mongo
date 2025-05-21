55abdfc87575786ae20b89ca20ec8d25785c367b
Marko Piric <marko.piric@edu.gbssg.ch>
2025-05-21 14:53:46 +0200

    02-AA-Minimal-API-MongoDB

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,29 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) =>
+{
+    try
+    {
+        var mongoDbConnectionString = options.Value.ConnectionString;
+
+        var client = new MongoClient(mongoDbConnectionString);
+
+        var databaseNames = client.ListDatabaseNames().ToList();
+
+        var dbList = string.Join(", ", databaseNames);
+        return $"Zugriff auf MongoDB ok. Gefundene Datenbanken: {dbList}";
+    }
+    catch (Exception ex)
+    {
+        return $"Fehler beim Zugriff auf MongoDB: {ex.Message}";
+    }
+});
 
 app.Run();
